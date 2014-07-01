2956e1d78c9f380c69d45429c76869a361f81771
William Yeh <william.pjyeh@gmail.com>
2014-07-01 18:50:00 +0800

    fix: minimize mongodb binaries

--- container-vs-vm/prepare/vagrant-provision.sh
+++ container-vs-vm/prepare/vagrant-provision.sh
@@ -10,7 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
-apt-get update  &&  apt-get install -y -q mongodb-org
+apt-get update  &&  apt-get install -y -q mongodb-org-server mongodb-org-shell
 apt-get clean
 
 #sudo /usr/sbin/invoke-rc.d mongod stop
