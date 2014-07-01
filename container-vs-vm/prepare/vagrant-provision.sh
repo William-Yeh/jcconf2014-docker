5b57c79d7da4515b4122d7b821a7083bfa8da243
William Yeh <william.pjyeh@gmail.com>
2014-07-01 12:07:15 +0800

    slim down mongodb packages

--- container-vs-vm/prepare/vagrant-provision.sh
+++ container-vs-vm/prepare/vagrant-provision.sh
@@ -10,10 +10,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
-apt-get update  &&  apt-get install -y -q mongodb-org
+apt-get update  &&  apt-get install -y -q mongodb-org-server mongodb-org-shell
 apt-get clean
 
-#sudo /usr/sbin/invoke-rc.d mongod stop
 sed -i -re 's/^\s*(bind_ip\s*=.*)/#\1\n#--> listen on all for vagrant host OS to access\nbind_ip=0.0.0.0/' /etc/mongod.conf
 
 
