ae873b38bb6bd55b456e09465dd34ee0c18f1a2f
William Yeh <william.pjyeh@gmail.com>
2014-07-02 17:31:48 +0800

    move container-vs-vm directory to separate repository 'codedata-docker-tutorial'

--- container-vs-vm/prepare/prepare-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-IMAGE_NAME=`cat DOCKER-IMAGE-NAME`
-
-sudo docker build -t $IMAGE_NAME .
-sudo docker images
--- container-vs-vm/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-vagrant halt
-vagrant destroy
-rm -rf .vagrant
-
-sudo docker stop db1 db2
-sudo docker rm `sudo docker ps --no-trunc -a -q`
