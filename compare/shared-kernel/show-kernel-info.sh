636c81ee95132a4ec1a74c8869565f6639591e88
William Yeh <william.pjyeh@gmail.com>
2014-10-04 00:01:43 +0800

    Enhance show-kernel-info logics and details

--- compare/shared-kernel/show-kernel-info.sh
+++ compare/shared-kernel/show-kernel-info.sh
@@ -2,11 +2,26 @@
 #
 # Simple tool to display info about Linux kernel and distribution.
 #
+# Usage:
+#
+#   - Summary: 
+#     ./show-kernel-info.sh | head -n 13
+#
+#   - Details:
+#     ./show-kernel-info.sh
+#     
+#   - Details with title:
+#     ./show-kernel-info.sh  "Simple demo"
+#
 
 OUTPUT_MSG=$1
 OS_VERSION=/etc/os-release
 REDHAT_RELEASE=/etc/redhat-release
 DEBIAN_VERSION=/etc/debian_version
+MAC_KERNEL=/mach_kernel
+
+
+export LC_ALL="en_US.UTF-8"
 
 
 echo $1
@@ -20,7 +35,37 @@ echo "--> [Kernel info]"
 uname -a
 
 echo
-echo "===== USERLAND ====="
+echo "===== USERLAND / Summary ====="
+
+echo
+echo "--> [Root file system viewpoint]"
+
+if [[ -e $MAC_KERNEL ]]; then
+    echo "Mac OS X"
+    stat -f "%Sm %N" $MAC_KERNEL
+
+elif [[ -e $REDHAT_RELEASE ]]; then
+    cat $REDHAT_RELEASE
+    stat -Lc "%y %N"  $REDHAT_RELEASE
+
+elif [[ -e $DEBIAN_VERSION ]]; then
+    cat $DEBIAN_VERSION
+    grep PRETTY_NAME /etc/os-release
+    stat -Lc "%y %N"  $OS_VERSION
+
+elif [[ -e $OS_VERSION ]]; then
+    echo "Other Linux compatible system"
+    grep PRETTY_NAME /etc/os-release
+    stat -Lc "%y %N"  $OS_VERSION
+
+else
+    echo "Unrecognized system"
+fi
+
+
+
+echo
+echo "===== USERLAND / Details ====="
 
 echo
 echo "--> [File info] Linux version in general"
